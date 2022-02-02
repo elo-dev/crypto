c71d133e6fbb4083f752903f5d7c06121c5c4471
elo-dev <dimasiys25@gmail.com>
2022-02-03 00:34:40 +0300

    Search crypto by name

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../../services/cryptoApi'
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Input, Row } from 'antd'
 import style from './Cryptocurrencies.module.scss'
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
-  if(isFetching) return 'Loading...'
+  useEffect(() => {
+    const filteredData = cryptosList?.data.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm])
+
+  if (isFetching) return 'Loading...'
 
   return (
     <>
+      {!simplified && (
+        <div className={style.search_crypto}>
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className={style.crypto_card_container}>
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className={style.crypto_card} key={currency.id}>
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className={style.crypto_card}
+            key={currency.id}
+          >
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className={style.crypto_image} src={currency.iconUrl} />}
+                extra={
+                  <img className={style.crypto_image} src={currency.iconUrl} />
+                }
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
